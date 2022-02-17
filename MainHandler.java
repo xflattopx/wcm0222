aaa994fedc3af3bc202df1fecbfa25056dcef596
xflattopx <xflattopx@gmail.com>
2022-02-16 23:14:34 -0500

    Initial Commit

---
+++ MainHandler.java
@@ -0,0 +1,8 @@
+// Point of Sale Tool Rental Tool
+// Author - Chase Moore
+
+public class MainHandler {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
