1e4c20e0f6597c6e1d2f81dcca73e56681816e17
chasemoore <xflattopx@gmail.com>
2022-02-16 23:53:32 -0500

    configured junit testing, updated structure of project

---
+++ src/main/MainHandler.java
@@ -0,0 +1,10 @@
+package src.main;
+// Point of Sale Tool Rental Tool
+
+// Author - Chase Moore
+
+public class MainHandler {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
---
+++ src/test/AppTest.java
@@ -0,0 +1,13 @@
+package src.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.*;
+
+public class AppTest {
+
+    @Test
+    public void test() {
+        assertEquals(5, 2 + 3);
+    }
+}
