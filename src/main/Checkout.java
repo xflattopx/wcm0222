4d35c6354395942f8bf7944afcc6255f1a2654fa
chasemoore <xflattopx@gmail.com>
2022-02-19 15:32:02 -0500

    added required test cases. Calculates total without considering dates, holidays or charge days.

--- src/main/Checkout.java
+++ src/main/Checkout.java
@@ -5,6 +5,8 @@
 //  Discount percent: As a whole number, 0-100 (e.g. 20 = 20%)
 //  Check out date
 
+import java.util.Date;
+
 import java.util.*;
 
 public class Checkout {
@@ -13,6 +15,11 @@ public class Checkout {
     List<Tool> tools;
     double discount;
     String date;
+    double total = 0;
+
+    public Checkout() {
+
+    }
 
     public Checkout(List<Tool> tools) {
         this.tools = tools;
@@ -24,16 +31,19 @@ public class Checkout {
         int input;
         optionPanel();
         input = scanner.nextInt();
-        System.out.println("input ->" + input);
         while (input != 9) {
             if (input == 1) {
                 providedInformation();
+                optionPanel();
                 input = scanner.nextInt();
             } else if (input == 2) {
                 optionPanel();
                 input = scanner.nextInt();
             } else if (input == 3) {
-                break;
+                optionPanel();
+                System.out.println("Running total: " + total);
+                input = scanner.nextInt();
+
             } else {
                 System.out.println("Invalid Input");
                 optionPanel();
@@ -49,6 +59,7 @@ public class Checkout {
         System.out.println("======================");
         System.out.println("To Checkout a Tool - 1");
         System.out.println("Return to Menu     - 2");
+        System.out.println("To View Total      - 3");
         System.out.println("Exit               - 9");
         System.out.println("======================");
 
@@ -56,17 +67,70 @@ public class Checkout {
 
     // ProvidedInformation() Collects the following information:
     // Tool code, rental day count, discount percent, checkout date
+    @Deprecated
     public void providedInformation() {
         // Date currentDate = new Date();
-        String input = "";
+        String code;
+        int month;
+        int day;
+        int year;
+        int days;
+        int discount;
+
         System.out.println("Please provide tool code: ");
-        input = scanner.next();
+        code = scanner.next();
         System.out.println("How many days would you like to rent: ");
-        input = scanner.next();
-        System.out.println("Percentage of discount: ");
-        input = scanner.next();
-        System.out.println("Day of checkout");
-        input = scanner.next();
+        days = scanner.nextInt();
+        System.out.println("Percentage of discount [between 0 - 100]: ");
+        discount = scanner.nextInt();
+        System.out.println("Month of checkout [0-11]");
+        month = scanner.nextInt();
+        System.out.println("Day of checkout [1-31]");
+        day = scanner.nextInt();
+        System.out.println("Year of checkout [1900 - 2022]");
+        year = scanner.nextInt();
+        total += calculateItemTotal(code, new Date(year, month, day), days, discount);
+
+    }
+
+    // calculateItemTotal() calculates the total of a tool given the following:
+    // Tool Code: A string uniquly identify the item.
+    // Checkout Date: A date in which the customer intends on checking item out.
+    // Rental Days: The number of days the customer intends on renting tool.
+    // Discount: A discount in which applies to the total
+    public double calculateItemTotal(String toolCode, Date checkoutDate, int rentalDays, double discount) {
+        double disPercentage = discount / 100; // Taken off the total after checkout
+        // int chargeDays = 0; // Total number of chargeable Days.
+        // Date dueDate;
+        double discountAmount = 0.0;
+        double itemTotal = 0.0;
+        Tool tool = new Tool();
+
+        // determine which tool we're using (can be improved with key value pair)
+        for (int i = 0; i < tools.size(); i++) {
+            // We found the tool.
+            if (tools.get(i).toolCode.equals(toolCode)) {
+                tool = tools.get(i);
+                break;
+            }
+        }
+
+        if (tool.toolCode.equals(null)) {
+            System.out.println("Invalid Tool Code");
+            return itemTotal;
+        }
+
+        if (disPercentage > -1 || disPercentage < 101)
+            discountAmount = (tool.type.price * rentalDays) * disPercentage;
+        else
+            return itemTotal;
+
+        itemTotal = (rentalDays * tool.type.price) - discountAmount;
+
+        System.out.println("item Price: " + tool.type.price + " Discount: " + discountAmount + " Total: " +
+                itemTotal);
+
+        return itemTotal;
     }
 
 }
--- src/test/AppTest.java
+++ src/test/AppTest.java
@@ -1,13 +1,68 @@
-package src.test;
 
 import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import org.junit.*;
 
 public class AppTest {
 
+    double delta = 0.001; // Expecte dand Result should be within these bounds.
+
     @Test
-    public void test() {
-        assertEquals(5, 2 + 3);
+    @Deprecated
+    public void checkoutTestCase1() {
+        List<Tool> tools = new ArrayList<Tool>();
+        Checkout checkout = new Checkout(tools);
+        tools.add(new Tool("JAKR", new ToolType("Jackhammer", 2.99, true, false, false)));
+        assertEquals("0.0", checkout.calculateItemTotal("JAKR", new Date(2015, 8, 3), 5, 101), delta);
+    }
+
+    @Test
+    @Deprecated
+    public void checkoutTestCase2() {
+        List<Tool> tools = new ArrayList<Tool>();
+        Checkout checkout = new Checkout(tools);
+        tools.add(new Tool("LADW", new ToolType("Chainsaw", 1.49, true, false, true)));
+        assertEquals(0, checkout.calculateItemTotal("LADW", new Date(2020, 6, 2), 3, 10), delta);
+
+    }
+
+    @Test
+    @Deprecated
+    public void checkoutTestCase3() {
+        List<Tool> tools = new ArrayList<Tool>();
+        Checkout checkout = new Checkout(tools);
+        tools.add(new Tool("CHNS", new ToolType("Ladder", 1.99, true, true, false)));
+        assertEquals(0, checkout.calculateItemTotal("CHNS", new Date(2020, 6, 2), 3, 10), delta);
+    }
+
+    @Test
+    @Deprecated
+    public void checkoutTestCase4() {
+        List<Tool> tools = new ArrayList<Tool>();
+        Checkout checkout = new Checkout(tools);
+        tools.add(new Tool("JAKD", new ToolType("Jackhammer", 2.99, true, false, false)));
+        assertEquals(0, checkout.calculateItemTotal("JAKD", new Date(2015, 8, 3), 3, 10), delta);
+    }
+
+    @Test
+    @Deprecated
+    public void checkoutTestCase5() {
+        List<Tool> tools = new ArrayList<Tool>();
+        Checkout checkout = new Checkout(tools);
+        tools.add(new Tool("JAKR", new ToolType("Jackhammer", 2.99, true, false, false)));
+        assertEquals(0, checkout.calculateItemTotal("JAKR", new Date(2015, 6, 2), 3, 10), delta);
+    }
+
+    @Test
+    @Deprecated
+    public void checkoutTestCase6() {
+        List<Tool> tools = new ArrayList<Tool>();
+        Checkout checkout = new Checkout(tools);
+        tools.add(new Tool("JAKR", new ToolType("Jackhammer", 2.99, true, false, false)));
+        assertEquals(0, checkout.calculateItemTotal("JAKR", new Date(2020, 6, 2), 3, 10), delta);
     }
 }
